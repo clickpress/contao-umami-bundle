d66eeef1a096d6d2f503f37706ff47587e1a73be
Stefan Schulz-Lauterbach <ssl@clickpress.de>
2025-12-10 23:39:55 +0100

    Add Umami URL field to tl_page and update translations

--- contao/dca/tl_page.php
+++ contao/dca/tl_page.php
@@ -7,8 +7,16 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['umami_id'] = [
     'sql' => ['type' => 'string', 'length' => 255, 'default' => ''],
 ];
 
+$GLOBALS['TL_DCA']['tl_page']['fields']['umami_url'] = [
+    'inputType' => 'text',
+    'eval' => ['tl_class' => 'w50', 'maxlength' => 255],
+    'sql' => ['type' => 'string', 'length' => 255, 'default' => ''],
+];
+
 PaletteManipulator::create()
-    ->addField('umami_id', 'website_legend', PaletteManipulator::POSITION_APPEND)
+    ->addLegend('umami_legend', 'website_legend', PaletteManipulator::POSITION_AFTER)
+    ->addField('umami_id', 'umami_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField('umami_url', 'umami_legend', PaletteManipulator::POSITION_APPEND)
     ->applyToPalette('root', 'tl_page')
     ->applyToPalette('rootfallback', 'tl_page')
 ;
