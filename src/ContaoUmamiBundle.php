0a3a5a1d7a7101d48e7308ce9a87bf4daacad3bb
Stefan Schulz-Lauterbach <ssl@clickpress.de>
2025-12-10 22:15:50 +0100

    Register services for ContaoUmamiBundle with YAML configuration

--- src/ContaoUmamiBundle.php
+++ src/ContaoUmamiBundle.php
@@ -2,9 +2,18 @@
 
 namespace Clickpress\ContaoUmamiBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class ContaoUmamiBundle extends AbstractBundle
 {
-
+    public function loadExtension(
+        array $config,
+        ContainerConfigurator $container,
+        ContainerBuilder $builder,
+    ): void
+    {
+        $container->import('../config/services.yaml');
+    }
 }
