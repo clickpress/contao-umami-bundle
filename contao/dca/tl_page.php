b7ce3f3d58b06caf9e05516134bc310773b7ab16
Stefan Schulz-Lauterbach <ssl@clickpress.de>
2025-12-10 22:15:57 +0100

    Add Umami API key field to tl_page data container

---
+++ contao/dca/tl_page.php
@@ -0,0 +1,14 @@
+<?php
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+
+$GLOBALS['TL_DCA']['tl_page']['fields']['umami_api_key'] = [
+    'label' => ['Umami API-Key', 'Enter the Umami API-Key here.'],
+    'inputType' => 'text',
+    'eval' => ['tl_class' => 'w50', 'maxlength' => 255],
+    'sql' => ['type' => 'string', 'length' => 255, 'default' => ''],
+];
+
+PaletteManipulator::create()
+    ->addField('umami_api_key', 'website_legend', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('rootfallback', 'tl_page')
+;
