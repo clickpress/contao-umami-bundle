5d88032267c4af8eee4960f2aeab8e92b5fedf3c
Stefan Schulz-Lauterbach <ssl@clickpress.de>
2025-12-11 00:04:08 +0100

    Add ContaoUmamiListener to inject Umami analytics script into frontend pages

---
+++ src/EventListener/ContaoUmamiListener.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Clickpress\ContaoUmamiBundle\EventListener;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\PageModel;
+
+#[AsHook('modifyFrontendPage')]
+class ContaoUmamiListener
+{
+
+    public function __invoke(string $buffer, string $templateName): string
+    {
+        if (strpos($templateName, 'fe_', 0) !== 0) {
+            return $buffer;
+        }
+
+        global $objPage;
+        $objRootPage = PageModel::findByPk($objPage->rootId);
+
+        // check if null, to prevent that the bar is loaded, even if the checkbox 'activate clickskeks' is not checked
+        if (null === $objRootPage || null === $objRootPage->umami_id || null === $objRootPage->umami_url) {
+            return $buffer;
+        }
+
+        if (!str_starts_with($objRootPage->umami_url, 'http')) {
+            $objRootPage->umami_url = 'https://' . $objRootPage->umami_url;
+        }
+
+        $html = sprintf(
+            '<script defer src="%s/script.js" data-website-id="%s"></script>',
+            $objRootPage->umami_url,
+            $objRootPage->umami_id
+        );
+
+        return preg_replace('/(<head>)/s', "$1\n$html", $buffer);
+    }
+}
