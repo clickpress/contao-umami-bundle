8f9a23bf6bfbcc5b7693865b29cf93e5078e2b0a
Stefan Schulz-Lauterbach <ssl@clickpress.de>
2025-12-10 23:35:30 +0100

    Rename Umami API key field to Umami ID in tl_page data container

--- contao/dca/tl_page.php
+++ contao/dca/tl_page.php
@@ -1,15 +1,14 @@
 <?php
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
-$GLOBALS['TL_DCA']['tl_page']['fields']['umami_api_key'] = [
-    'label' => ['Umami API-Key', 'Enter the Umami API-Key here.'],
+$GLOBALS['TL_DCA']['tl_page']['fields']['umami_id'] = [
     'inputType' => 'text',
     'eval' => ['tl_class' => 'w50', 'maxlength' => 255],
     'sql' => ['type' => 'string', 'length' => 255, 'default' => ''],
 ];
 
 PaletteManipulator::create()
-    ->addField('umami_api_key', 'website_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField('umami_id', 'website_legend', PaletteManipulator::POSITION_APPEND)
     ->applyToPalette('root', 'tl_page')
     ->applyToPalette('rootfallback', 'tl_page')
 ;
